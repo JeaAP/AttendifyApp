59a0990d1a151f3227473206d4768d95700942b1
Mosuvy <andhikadaninatawidjaja@gmail.com>
2025-01-01 21:47:51 +0700

    scan

---
+++ app/src/main/java/com/example/attendify/WebViewActivity.kt
@@ -0,0 +1,31 @@
+package com.example.attendify
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.attendify.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        val url = intent.getStringExtra("url")
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        if(url != null) {
+            webView.loadUrl(url)
+        }
+    }
+}
