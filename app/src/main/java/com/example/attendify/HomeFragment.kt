a2dee54148e29b810ddd572c71b5a258f7b997f5
Mosuvy <andhikadaninatawidjaja@gmail.com>
2025-01-02 16:29:57 +0700

    benerin benerinnnn

--- app/src/main/java/com/example/attendify/HomeFragment.kt
+++ app/src/main/java/com/example/attendify/HomeFragment.kt
@@ -2,17 +2,22 @@ package com.example.attendify
 
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer.OnPreparedListener
 import android.os.Bundle
 import android.os.Looper
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.attendify.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.logging.Handler
 
 class HomeFragment : Fragment() {
 
@@ -23,9 +28,11 @@ class HomeFragment : Fragment() {
     private lateinit var timeUpdater: Runnable
 
     interface FragmentInteractionListener {
+//        fun onButtonClicked()
         fun updateLocationText(text: String)
         fun isUserInGeofence(): Boolean
     }
+
 
     private var listener: FragmentInteractionListener? = null
     private var currentLocation: String? = null
@@ -38,7 +45,8 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
+        // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater)
         dbHelperProfile = DatabaseHelperProfile(requireContext())
         dbHelperAbsensi = DatabaseHelperAbsensi(requireContext())
@@ -48,29 +56,53 @@ class HomeFragment : Fragment() {
         }
         updateLocationText(currentLocation ?: "Loading location...")
 
+        // Load data from the database and update UI
         loadProfileData()
 
+        // Set listeners for buttons and views
         binding.FotoProfile.setOnClickListener {
-            val intent = Intent(requireContext(), ProfileActivity::class.java)
+            val intent = Intent(this@HomeFragment.requireContext(), ProfileActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnAbcent.setOnClickListener {
-            handleAttendance()
+            val now = Calendar.getInstance()
+            val cutOffTime = Calendar.getInstance()
+            cutOffTime.set(Calendar.HOUR_OF_DAY, 6)
+            cutOffTime.set(Calendar.MINUTE, 30)
+
+            if (listener?.isUserInGeofence() == true) {
+                val today = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+                if (!dbHelperAbsensi.hasAbsensiToday(today)) {
+                    if(now.after(cutOffTime)){
+                        Toast.makeText(context, "Anda tidak bisa absen setelah pukul 06:30", Toast.LENGTH_LONG).show()
+                        return@setOnClickListener
+                    } else{
+                        val intent = Intent(this@HomeFragment.requireContext(), ScanActivity::class.java)
+                        startActivity(intent)
+                    }
+                } else {
+                    Toast.makeText(context, "Anda sudah melakukan absen hari ini", Toast.LENGTH_LONG).show()
+                }
+//                val intent = Intent(this@HomeFragment.requireContext(), ScanActivity::class.java)
+//                startActivity(intent)
+            } else {
+                Toast.makeText(context, "Anda harus berada di dalam wilayah SMKN 24 Jakarta", Toast.LENGTH_LONG).show()
+            }
         }
 
         binding.btnIzin.setOnClickListener {
-            val intent = Intent(requireContext(), coomingSoon::class.java)
+            val intent = Intent(this@HomeFragment.requireContext(), coomingSoon::class.java)
             startActivity(intent)
         }
 
         binding.cardSchedule.setOnClickListener {
-            val intent = Intent(requireContext(), coomingSoon::class.java)
+            val intent = Intent(this@HomeFragment.requireContext(), coomingSoon::class.java)
             startActivity(intent)
         }
 
         binding.linkText.setOnClickListener {
-            val intent = Intent(requireContext(), viewAllAbsensi::class.java)
+            val intent = Intent(this@HomeFragment.requireContext(), viewAllAbsensi::class.java)
             startActivity(intent)
         }
 
@@ -96,17 +128,16 @@ class HomeFragment : Fragment() {
         outState.putString("CURRENT_LOCATION", binding.location.text.toString())
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        handler.removeCallbacks(timeUpdater) // Hentikan pembaruan saat fragment dihancurkan
+    fun updateLocationText(text: String) {
+        binding.location.text = text
+        currentLocation = text
     }
 
     private fun setupTimeUpdater() {
         timeUpdater = object : Runnable {
             override fun run() {
                 updateCurrentTime()
-                updateDate() // Perbarui tanggal secara terus-menerus
-                handler.postDelayed(this, 60000) // Jalankan setiap menit
+                handler.postDelayed(this, 60000)  // Update time every minute
             }
         }
         handler.post(timeUpdater)
@@ -119,14 +150,9 @@ class HomeFragment : Fragment() {
     }
 
     private fun updateDate() {
-        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.ENGLISH)
         val currentDate = dateFormat.format(Date())
         binding.date.text = currentDate
-    }
-
-    private fun updateLocationText(text: String) {
-        binding.location.text = text
-        currentLocation = text
     }
 
     private fun loadProfileData() {
@@ -146,35 +172,13 @@ class HomeFragment : Fragment() {
             } else {
                 binding.FotoProfile.setImageResource(R.drawable.account_circle)
             }
+
         } else {
             binding.accountName.text = "[Nama tidak ditemukan]"
             binding.accountClass.text = "[Kelas tidak ditemukan]"
             binding.username.text = "Hi [Nama],"
             binding.greetings.text = "[Greetings]"
             binding.motivations.text = "[Motivasi]"
-        }
-    }
-
-    private fun handleAttendance() {
-        val now = Calendar.getInstance()
-        val cutOffTime = Calendar.getInstance()
-        cutOffTime.set(Calendar.HOUR_OF_DAY, 6)
-        cutOffTime.set(Calendar.MINUTE, 30)
-
-        if (listener?.isUserInGeofence() == true) {
-            val today = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-            if (!dbHelperAbsensi.hasAbsensiToday(today)) {
-                if (now.after(cutOffTime)) {
-                    Toast.makeText(context, "Anda tidak bisa absen setelah pukul 06:30", Toast.LENGTH_LONG).show()
-                } else {
-                    val intent = Intent(requireContext(), ScanActivity::class.java)
-                    startActivity(intent)
-                }
-            } else {
-                Toast.makeText(context, "Anda sudah melakukan absen hari ini", Toast.LENGTH_LONG).show()
-            }
-        } else {
-            Toast.makeText(context, "Anda harus berada di dalam wilayah SMKN 24 Jakarta", Toast.LENGTH_LONG).show()
         }
     }
 }
